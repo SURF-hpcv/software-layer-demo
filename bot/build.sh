17ddd4d6b37722b796e3f7c2fa4b75b67140cf1d
Caspar van Leeuwen <casparl@int5.local.snellius.surf.nl>
2026-05-29 14:55:32 +0200

    Add bot/build.sh

---
+++ bot/build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# give up as soon as any error occurs
+set -e
+
+git clone -b improve_building_on_top_support https://github.com/SURF-hpcv/software-layer-scripts
+
+# symlink everything, except for:
+# - common files like LICENSE and README.md
+# - 'bot' subdirectory, there we need to be a bit more careful (see below)
+for file in $(ls software-layer-scripts | egrep -v 'easystacks|LICENSE|README.md|^bot'); do
+    ln -s software-layer-scripts/${file}
+done
+
+# symlink all scripts in 'bot' subdirectory, except for bot/build.sh
+for file in $(ls software-layer-scripts/bot | grep -v '^build.sh'); do
+    ln -s ../software-layer-scripts/bot/${file} bot/${file}
+done
+
+# call out to bot/build.sh script from software-layer-scripts
+software-layer-scripts/bot/build.sh
